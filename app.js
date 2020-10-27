79c04673f49ad1f9b0fab12aaadc5f07569b5233
WaliEEE <jocularriad2@gmail.com>
2020-10-27 23:02:53 +0600

    Final Commit

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const bParser = require("body-parser");
-const request = require("request");
 const https = require("https");
+const request = require("request");
 const app = express();
 
-app.use(express.static("public"));
-app.use(bParser.urlencoded({ extended: true }));
+app.use(express.static("public"));     //using static folder(public) 
+app.use(bParser.urlencoded({ extended: true })); //using body-parser 
 app.listen(3000, (req, res) => 
 {
-    console.log("Hello Everyone")
+    console.log("Working Server")
 })
 
-
+//response to Display html file
 app.get("/", (req,res) => 
 {
     res.sendFile(`${__dirname}/index.html`)
 })
 
+
+//response after post
 app.post("/", (req, res) => 
 {
   const firstName = req.body.fName;
-  const lastName = req.body.lName;
+  const lastName  = req.body.lName;
   const email = req.body.email;
 
+// api list id from audience/subscriber
+const url = 'https://us2.api.mailchimp.com/3.0/lists/bd6e56024f' 
+const options=
+{
+  method: "POST",
+  auth: "Wali:e7d29b1ddb8da01dc9c93aac08d4c46e-us2" //authenticate
+}
+
+  //api reference for subscriber object list 
   const data = 
   {
     members: 
@@ -38,27 +49,36 @@ app.post("/", (req, res) =>
       },
     ],
   };
+
+  //collect data and json it
   const jsonData= JSON.stringify(data)
 
-  const url = 'https://us2.api.mailchimp.com/3.0/lists/bd6e56024f' // list id from audience
-
-  const options=
-  {
-    method: "POST",
-    auth: "Wali:e7d29b1ddb8da01dc9c93aac08d4c46e-us2"
-  }
-
+  //now work with formatted json data
   const request = https.request(url, options, (response) => 
   {
+    //callback
+    if (response.statusCode === 200) 
+    res.sendFile(`${__dirname}/success.html`)
+    else 
+    res.sendFile(`${__dirname}/failure.html`)
+   
+    //not imp
     response.on("data", (data) => 
     {
-     console.log("Success")
-    })
-  })
+      console.log("Success");
+    });
+  });
 
+  //multiple write
   request.write(jsonData)
   request.end()
 });
+
+//if fails redirect to home page through danger route
+app.post("/danger", (req, res) => 
+{
+  res.redirect("/")
+})
 
 
 //api keys : e7d29b1ddb8da01dc9c93aac08d4c46e-us2  list_id: bd6e56024f
